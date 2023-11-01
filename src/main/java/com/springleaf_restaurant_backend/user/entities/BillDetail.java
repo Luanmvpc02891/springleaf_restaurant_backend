cdc0ffb603b02a2f55d15b7ffbb5b42d4525db54
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-11-02 02:11:29 +0700

    merge main

--- src/main/java/com/springleaf_restaurant_backend/user/entities/BillDetail.java
+++ src/main/java/com/springleaf_restaurant_backend/user/entities/BillDetail.java
@@ -1,7 +1,14 @@
 package com.springleaf_restaurant_backend.user.entities;
 
-import lombok.*;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
@@ -9,6 +16,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "Bill_Details")
 public class BillDetail {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "bill_detail_id")
