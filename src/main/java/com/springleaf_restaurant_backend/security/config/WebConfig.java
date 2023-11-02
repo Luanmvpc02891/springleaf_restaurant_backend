6086e6a9749650528ac7dfde5169144c99ce64d4
Lê Trung Hiếu <hieult808@gmail.com>
2023-11-02 19:28:59 +0700

    Mở coment + config url

--- src/main/java/com/springleaf_restaurant_backend/security/config/WebConfig.java
+++ src/main/java/com/springleaf_restaurant_backend/security/config/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin("https://springleafrestaurant.onrender.com");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
