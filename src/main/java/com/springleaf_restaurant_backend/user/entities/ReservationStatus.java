90dd06d4e01b87562bc7b03cb13f80dbdfeca447
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-11-20 00:26:33 +0700

    sửa tên reservationStatusName

--- src/main/java/com/springleaf_restaurant_backend/user/entities/ReservationStatus.java
+++ src/main/java/com/springleaf_restaurant_backend/user/entities/ReservationStatus.java
@@ -1,7 +1,14 @@
 package com.springleaf_restaurant_backend.user.entities;
 
-import lombok.*;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
@@ -16,6 +23,6 @@ public class ReservationStatus {
     private Integer reservationId;
 
     @Column(name = "reservation_status_name")
-    private String restaurantTableId;
+    private String reservationStatusName;
 
 }
