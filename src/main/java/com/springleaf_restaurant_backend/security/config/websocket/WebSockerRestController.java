987672e00e56f40d63beb3c0c4cb73085773728a
maiquocbao666 <maiquocbao666@gmail.com>
2023-11-22 13:46:16 +0700

    Phân biệt chuỗi và đối tượng websocket

--- src/main/java/com/springleaf_restaurant_backend/security/config/websocket/WebSockerRestController.java
+++ src/main/java/com/springleaf_restaurant_backend/security/config/websocket/WebSockerRestController.java
@@ -11,12 +11,23 @@ import org.springframework.web.util.HtmlUtils;
 @Controller
 public class WebSockerRestController {
 
+    //Trả về đối tượng
+    // @MessageMapping("/hello")
+    // @SendTo("/topic/greetings")
+    // public Greeting greeting(HelloMessage message) throws Exception {
+    //     //Thread.sleep(1000); // simulated delay
+    //     System.out.println(message.getName());
+    //     return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    // }
+
+    //Trả về chuỗi
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
+    public String greeting(HelloMessage message) throws Exception {
         //Thread.sleep(1000); // simulated delay
         System.out.println(message.getName());
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        // return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        return "Hello, " + message.getName() + "!";
     }
 
 }
