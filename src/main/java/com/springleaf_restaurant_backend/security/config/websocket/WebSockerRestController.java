b5d8be54e1c72a67324df35fc7f0f347e4fcc337
maiquocbao666 <maiquocbao666@gmail.com>
2023-11-22 13:32:57 +0700

    Bỏ sleep

--- src/main/java/com/springleaf_restaurant_backend/security/config/websocket/WebSockerRestController.java
+++ src/main/java/com/springleaf_restaurant_backend/security/config/websocket/WebSockerRestController.java
@@ -14,7 +14,7 @@ public class WebSockerRestController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
+        //Thread.sleep(1000); // simulated delay
         System.out.println(message.getName());
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
