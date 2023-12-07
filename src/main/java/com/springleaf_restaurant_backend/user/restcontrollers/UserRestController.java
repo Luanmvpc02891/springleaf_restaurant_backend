66f5909fd368f5443414067d17aba75ed5ddb223
maiquocbao666 <maiquocbao666@gmail.com>
2023-12-08 03:11:21 +0700

    hiển thị user

--- src/main/java/com/springleaf_restaurant_backend/user/restcontrollers/UserRestController.java
+++ src/main/java/com/springleaf_restaurant_backend/user/restcontrollers/UserRestController.java
@@ -3,6 +3,7 @@ package com.springleaf_restaurant_backend.user.restcontrollers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import com.springleaf_restaurant_backend.security.entities.User;
@@ -12,13 +13,27 @@ import com.springleaf_restaurant_backend.security.repositories.UserRepository;
 public class UserRestController {
     @Autowired
     private UserRepository userRepository;
-    
+
     PasswordEncoder passwordEncoder;
 
-    @GetMapping("/admin/users")
-    public List<User> getUser() {
-        return userRepository.findAll();
+    @PostMapping("/manager/users")
+    public ResponseEntity<List<User>> getUser(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(userRepository.findAll());
     }
 
-    
+    @PutMapping("/manager/update/users")
+    public ResponseEntity<User> createUser(
+            @RequestBody User user,
+            @RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+
+    // @DeleteMapping("/manager/delete/user/{id}")
+    // public ResponseEntity<User> deleteUser(
+    // @RequestBody User user,
+    // @RequestHeader("Authorization") String token
+    // ) {
+    // return ResponseEntity.ok(userRepository.save(user));
+    // }
+
 }
