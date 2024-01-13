edf0a469750b088a4057c5cf1f5c8c0e1a8e45e1
maiquocbao666 <maiquocbao666@gmail.com>
2024-01-13 09:21:19 +0700

    floor

---
+++ src/main/java/com/springleaf_restaurant_backend/user/entities/Floor.java
@@ -0,0 +1,24 @@
+package com.springleaf_restaurant_backend.user.entities;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Floors")
+public class Floor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "restaurant_id")
+    private Long restaurantId;
+
+}
