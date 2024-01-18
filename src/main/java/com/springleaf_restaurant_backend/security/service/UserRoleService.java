ec4696c0a30ea276359859303f5c4e48dcc4641f
Lê Trung Hiếu <hieult808@gmail.com>
2024-01-18 11:08:11 +0700

    Cấp quyền người dùng

--- src/main/java/com/springleaf_restaurant_backend/security/service/UserRoleService.java
+++ src/main/java/com/springleaf_restaurant_backend/security/service/UserRoleService.java
@@ -3,10 +3,11 @@ package com.springleaf_restaurant_backend.security.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.springleaf_restaurant_backend.security.entities.Role;
 import com.springleaf_restaurant_backend.security.entities.UserRole;
 
 public interface UserRoleService {
-    UserRole createUserRole(UserRole userRole);
+    UserRole createUserRole(UserRole updatedTable);
     List<UserRole> getAllUserRoles();
     Optional<UserRole> getUserRoleById(Long userRoleId);
     UserRole updateUserRole(Long userRoleId, UserRole userRole);
